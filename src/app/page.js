1c182f6c9c98b6cb2c393fc0b4718fd2546e1dd7
asrafilll <asrafilr@gmail.com>
2023-09-16 22:21:19 +0700

    chore: add code branch to page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/shared-ui/Button";
 export default function Page() {
   return (
     <div>
-      <div>EventMakers-Nanosync</div>
+      <div>EventMakers-Nanosync #branch 4</div>
       <Button variant="primary">Click me</Button>
     </div>
   );
