040bd4208e20fc0ef4f5bbbb4e6ae887b28d2535
asrafilll <asrafilr@gmail.com>
2023-09-18 22:18:30 +0700

    chore: Add empty div for register page and clear return for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
 import { Button } from "@/components/shared-ui/Button";
 
 export default function Page() {
-  return (
-    <div>
-      <div>EventMakers-Nanosync #branch 4</div>
-      <Button variant="primary">Click me</Button>
-    </div>
-  );
+  return <div></div>;
 }
